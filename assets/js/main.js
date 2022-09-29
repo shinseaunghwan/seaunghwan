4088f7f7b57073c234c362e9317106ff8abcc2c2
shinseaunghwan <hanan0912@icloud.com>
2022-09-29 15:03:09 +0900

    portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -173,37 +173,26 @@
 
 
 
-  // /**
-  //  * Testimonials slider
-  //  */
-  // new Swiper('.services-slider', {
-  //   speed: 600,
-  //   loop: true,
-  //   autoplay: {
-  //     delay: 5000,
-  //     disableOnInteraction: false
-  //   },
-  //   slidesPerView: 'auto',
-  //   pagination: {
-  //     el: '.swiper-pagination',
-  //     type: 'bullets',
-  //     clickable: true
-  //   },
-  //   breakpoints: {
-  //     320: {
-  //       slidesPerView: 1,
-  //       spaceBetween: 20
-  //     },
-  //     768: {
-  //       slidesPerView: 2,
-  //       spaceBetween: 20
-  //     },
-  //     1200: {
-  //       slidesPerView: 4,
-  //       spaceBetween: 20
-  //     }
-  //   }
-  // });
+/**
+   * about Slider
+   */
+  new Swiper('.about-slider', {
+  speed: 400,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false
+  },
+  effect: 'coverflow',
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  slidesPerView: 'auto',
+  breakpoints: {
+    slidesPerView: 1,
+}
+});
 
   /**
    * Porfolio isotope and filter
